bec19b1da619e946643a3b518cdb04b83ccc75f4
Alejandro <jandro240@gmail.com>
2014-03-26 13:45:37 -0500

    filtro insectos negros

--- src/plagas.cpp
+++ src/plagas.cpp
@@ -36,6 +36,9 @@ int main (void){
     cv::Mat image3;
     cv::Mat image4;
     cv::Mat image5;
+    cv::Mat image6;
+    cv::Mat image7;
+    cv::Mat image8;
     cv::Mat ROI;
   
     
@@ -56,25 +59,58 @@ int main (void){
 		ROI=image1(cv::Rect(500,1000,1000,800));
 		
 		
-// 				inRange(ROI,//matriz de entrada
-// 					Scalar(150,110,1),//limite inferior
-// 					Scalar(190,120,10),//limite superior
-// 					image4//matriz de salida
-// 				  
-// 				);
-				//cvtColor(ROI, image2, CV_BGR2XYZ);
-				//threshold(image2, image3, 150, 255 ,1);
-				//cvtColor(image3, image4, CV_BGR2GRAY);
-				//dilate(image4, image5,cv::Mat(),cv::Point(-1,-1),3);
-		inRange(ROI, Scalar(R_MIN,G_MIN,B_MIN), Scalar(R_MAX,G_MAX,B_MAX), image4);			
+//   				inRange(ROI,//matriz de entrada
+//   					Scalar(0,160,10),//limite inferior
+//   					Scalar(255,255,255),//limite superior
+//   					image4//matriz de salida
+//   				  
+//   				);
+				
+				
+				
+				cvtColor(ROI, image2, CV_BGR2XYZ);
+				imshow("1  color XYZ",image2);
+				//vamos a eliminar los insectos mas oscuros
+				      threshold(image2, image3, 93, 255 ,1);
+				      //threshold(image2, image3, R_MIN, R_MAX ,1);
+				      imshow("2  threshold despues de xyz",image3);
+				      
+				      cvtColor(image3, image4, CV_BGR2GRAY);
+				      imshow("3  escala de grises",image4);
+				      
+				      threshold(image4, image5, 82, 255 ,0);
+				      dilate(image5, image5,cv::Mat(),cv::Point(-1,-1),3);
+				      imshow("4  binario luego de grises",image5);
+				      threshold(image5, image5, 82, 255 ,1);
+				       
+				//inputMat.copyTo(outputMat, maskMat);
+				ROI.copyTo(image1, image5);					///**************ERROR******************
+				imshow("insectos negros eliminados",image1);
+				
+				inRange(image1,//matriz de entrada
+   					Scalar(5,160,10),//limite inferior
+   					Scalar(255,255,255),//limite superior
+   					image6//matriz de salida
+   				  
+   				);
+				 
+				 
+				 
+				//inRange(image1, Scalar(R_MIN,G_MIN,B_MIN), Scalar(R_MAX,G_MAX,B_MAX), image6);
+				imshow("inrange",image6);
+				//threshold(image4, image4, 0, 255 ,1);
+				//ROI.copyTo(image5, image4);
+				
+				
 			//threshold(image4, image5, 240, 255 ,0);
-			//Canny(image5,image1,100,200);
+			//Canny(image4,image1,100,200);
 			//erode(image2, image2,MORPH_RECT);
 			//dilate(image2, image2,cv::Mat(),cv::Point(-1,-1),3);
 			
-			imshow("camera",ROI);
-			imshow("XYZ",image4);
-			//imshow("Thresh",image2);
+// 			imshow("camera",ROI);
+// 			imshow("XYZ",image4);
+// 			imshow("mascar",image);
+				image1.~Mat();
 			waitKey(0);
 		}
 		
